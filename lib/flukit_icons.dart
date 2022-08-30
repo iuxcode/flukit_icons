fc35251bd4bd3e742f9795280ef31e466c36185c
charles9904 <charlottabade@gmail.com>
2022-08-30 00:16:38 +0000

    update main widget

--- lib/flukit_icons.dart
+++ lib/flukit_icons.dart
@@ -3,48 +3,80 @@ library flukit_icons;
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import 'model.dart';
+import 'icons.dart';
+// import 'model.dart';
 
 export 'icons.dart';
-export 'model.dart';
+// export 'model.dart';
 
 enum FluIconStyles { linear, broken, bulk, twotone }
 
+/* TODO Fix:
+  - Bulk Brand icons
+  - Mobile icon: Linear && Twotone
+  - Document copy: Twotone
+  - Document sketch: Twotone
+  - Task Square: Bulk
+  - Blend2: Linear && Twotone
+  - Brush: Bulk
+  - Recovery: Linear && Twotone
+  - CD: Linear && Twotone && Bulk
+  - Frame 1 & 2 & 3 & 4: Bulk
+  - QuoteDown: Broken
+  - VolumeCross: Twotone
+  - VolumeSlash: Twotone
+*/
+
 class FluIcon extends StatelessWidget {
-  final FluIconData icon;
+  final FluIcons icon;
   final FluIconStyles style;
   final Color? color;
   final double size;
   final double strokewidth;
   final Alignment alignment;
+  final EdgeInsets margin;
 
-  const FluIcon({
-    Key? key,
-    required this.icon,
-    this.style = FluIconStyles.twotone,
-    this.size = 24,
-    this.strokewidth = 1.5,
-    this.alignment = Alignment.center,
-    this.color,
-  }) : super(key: key);
+  const FluIcon(this.icon,
+      {Key? key,
+      this.style = FluIconStyles.twotone,
+      this.size = 24,
+      this.strokewidth = 1.5,
+      this.alignment = Alignment.center,
+      this.color,
+      this.margin = EdgeInsets.zero})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String data = icon.datas[style.name];
+    bool isUnicon = icon.category.toLowerCase() == 'unicon';
+    FluIconStyles _style = style;
 
-    return Align(
+    if (isUnicon &&
+        (style == FluIconStyles.broken || style == FluIconStyles.twotone)) {
+      _style = icon.paths[FluIconStyles.linear.name] != null
+          ? FluIconStyles.linear
+          : FluIconStyles.bulk;
+    }
+
+    String? path = icon.paths[_style.name];
+
+    if (path == null) {
+      print('Error: Style ${_style.name} doesn\'t exist for icon ${icon.name}');
+    }
+
+    return Container(
+      height: size,
+      width: size,
       alignment: alignment,
-      child: SizedBox(
-        height: size,
-        width: size,
-        child: RepaintBoundary(
-          child: SvgPicture.string(
-            '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">$data</svg>'
-                .replaceAll('stroke-width="1.5"', 'stroke-width="$strokewidth"'),
-            height: size,
-            width: size,
-            color: color ?? Colors.black,
-          ),
+      margin: margin,
+      child: RepaintBoundary(
+        child: SvgPicture.string(
+          '<svg width="24" height="24" viewBox="0 0 24 24" ${isUnicon ? '' : 'fill="none"'} xmlns="http://www.w3.org/2000/svg">${isUnicon ? '<g>%data%</g>' : '%data%'}</svg>'
+              .replaceAll('%data%', path ?? '')
+              .replaceAll('stroke-width="1.5"', 'stroke-width="$strokewidth"'),
+          height: size,
+          width: size,
+          color: color ?? Colors.black,
         ),
       ),
     );
